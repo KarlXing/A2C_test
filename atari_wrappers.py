85685339a3c515afa528838b04cd20d72a1c76da
Karl <jinweixing1006@gmail.com>
2018-11-30 09:40:15 -0800

    No downsample

--- atari_wrappers.py
+++ atari_wrappers.py
@@ -152,14 +152,14 @@ class WarpFrame(gym.ObservationWrapper):
     def __init__(self, env):
         """Warp frames to 84x84 as done in the Nature paper and later work."""
         gym.ObservationWrapper.__init__(self, env)
-        self.width = 84
-        self.height = 84
+        self.width = 210
+        self.height = 160
         self.observation_space = spaces.Box(low=0, high=255,
             shape=(self.height, self.width, 1), dtype=np.uint8)
 
     def observation(self, frame):
         frame = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
-        frame = cv2.resize(frame, (self.width, self.height), interpolation=cv2.INTER_AREA)
+        # frame = cv2.resize(frame, (self.width, self.height), interpolation=cv2.INTER_AREA)
         return frame[:, :, None]
 
 class WarpFrameCarl(gym.ObservationWrapper):
