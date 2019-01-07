8298031eae4368207f985f4e199495d4289585a0
Karl <jinweixing1006@gmail.com>
2019-01-07 10:54:18 -0800

    Use mean rather than smoothing

--- main.py
+++ main.py
@@ -126,7 +126,8 @@ def main():
     masks_device = torch.ones(args.num_processes, 1).to(device)
     #smooth_evaluations = torch.tensor(0.0)
     #max_smooth_evaluations = torch.tensor(0.0)
-    smooth_error = torch.tensor(0.0)
+    #smooth_error = torch.tensor(0.0)
+    mean_error = torch.tensor(0.0)
 
 
     rollouts = RolloutStorage(args.num_steps, args.num_processes,
@@ -166,8 +167,9 @@ def main():
             with torch.no_grad():
                 masks_device.copy_(masks)
                 next_value = actor_critic.get_value(obs, g_device, recurrent_hidden_states, masks_device).detach()
-            evaluations, g, pderror = update_mode(evaluations, masks, reward, value, next_value, tonic_g, phasic_g, g, args.phasic_threshold, smooth_error)
-            smooth_error = args.smooth_weight * smooth_error + (1 - args.smooth_weight) * pderror
+            evaluations, g, pderror = update_mode(evaluations, masks, reward, value, next_value, tonic_g, phasic_g, g, args.phasic_threshold, mean_error)
+            mean_error = (mean_error*(glob_step-1)+pderror)/glob_step
+            #smooth_error = args.smooth_weight * smooth_error + (1 - args.smooth_weight) * pderror
             #mean_evaluations = (mean_evaluations*(glob_step-1)+torch.mean(abs(evaluations)))/glob_step
             #smooth_evaluations = args.smooth_weight*smooth_evaluations + (1 - args.smooth_weight)*torch.mean(abs(evaluations))
             #max_smooth_evaluations = torch.max(max_smooth_evaluations, smooth_evaluations)
@@ -176,7 +178,7 @@ def main():
 
             if args.log_evaluation:
                 writer.add_scalar('analysis/evaluations', evaluations[0], j*args.num_steps + step)
-                writer.add_scalar('analysis/smooth_error', smooth_error, j*args.num_steps + step)
+                writer.add_scalar('analysis/mean_error', mean_error, j*args.num_steps + step)
                 if done[0]:
                     writer.add_scalar('analysis/done', 1, j*args.num_steps+step)
                 # writer.add_scalar('analysis/mean_evaluations', mean_evaluations, glob_step)
