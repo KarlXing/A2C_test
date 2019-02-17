d9a46b3cba9c53b5f94b19dbd37fca94b2f479bb
Karl <jinweixing1006@gmail.com>
2019-02-16 17:55:06 -0800

    Clean code

--- main.py
+++ main.py
@@ -144,34 +144,17 @@ def main():
             # Obser reward and next obs
             obs, reward, done, infos = envs.step(action)
 
-            # If done then clean the history of observations.
             masks = torch.FloatTensor([[0.0] if done_ else [1.0]
                                        for done_ in done])
 
             obs = obs_representation(obs, args.modulation, g_device, args.input_neuro)
 
-            #update g
-            with torch.no_grad():
-                masks_device.copy_(masks)
-                next_value, next_dist_entropy = actor_critic.get_value(obs, recurrent_hidden_states, masks_device)
-            # next_dist_entropy = next_dist_entropy.cpu().unsqueeze(1)
-            # combined_reward = reward + next_dist_entropy
             if args.log_evaluation:
                 writer.add_scalar('analysis/reward', reward[0], g_step)
-                # writer.add_scalar('analysis/entropy_reward', next_dist_entropy[0].item(), g_step)
-                # writer.add_scalar('analysis/evaluations', evaluations[0], g_step)
-                # writer.add_scalar('analysis/pd_error', pd_error[0], g_step)
-                # writer.add_scalar('analysis/g', g[0], g_step)
-                # writer.add_scalar('analysis/mean_evaluation', mean_evaluation, g_step)
-                # writer.add_scalar('analysis/xmax', xmax.cpu(), g_step)
-                # writer.add_scalar('analysis/xmin', xmin.cpu(), g_step)
-                # writer.add_scalar('analysis/xmean', xmean.cpu(), g_step)
                 writer.add_scalar('analysis/entropy', ori_dist_entropy[0].item(), g_step)
                 if done[0]:
                     writer.add_scalar('analysis/done', 1, g_step)
-                # for i in range(args.num_processes):
-                #     if done[i]:
-                #         writer.add_scalar('analysis/done', i, j*args.num_steps + step)
+
             for idx in range(len(infos)):
                 info = infos[idx]
                 if 'episode' in info.keys():
@@ -188,10 +171,10 @@ def main():
                         torch.save(save_model, os.path.join(save_path, args.env_name + ".pt"))                        
             assert(torch.sum(g_device).item() == args.num_processes)
             rollouts.insert(obs, recurrent_hidden_states, action, action_log_prob, value, reward, masks, g_device)
-        # with torch.no_grad():
-        #     next_value = actor_critic.get_value(rollouts.obs[-1],
-        #                                         rollouts.recurrent_hidden_states[-1],
-        #                                         rollouts.masks[-1]).detach()
+
+        with torch.no_grad():
+            masks_device.copy_(masks)
+            next_value, next_dist_entropy = actor_critic.get_value(obs, recurrent_hidden_states, masks_device)
 
         rollouts.compute_returns(next_value, args.use_gae, args.gamma, args.tau)
 
@@ -199,106 +182,6 @@ def main():
 
         rollouts.after_update()
 
-        # if j % args.save_interval == 0 and args.save_dir != "":
-        #     save_path = os.path.join(args.save_dir, args.algo)
-        #     try:
-        #         os.makedirs(save_path)
-        #     except OSError:
-        #         pass
-
-        #     # A really ugly way to save a model to CPU
-        #     save_model = actor_critic
-        #     if args.cuda:
-        #         save_model = copy.deepcopy(actor_critic).cpu()
-
-        #     save_model = [save_model,
-        #                   getattr(get_vec_normalize(envs), 'ob_rms', None)]
-
-        #     torch.save(save_model, os.path.join(save_path, args.env_name + ".pt"))
-
-        # total_num_steps = (j + 1) * args.num_processes * args.num_steps
-
-        # if args.log_interval is not None and j % args.log_interval == 0 and len(episode_rewards) > 1:
-        #     end = time.time()
-        #     print("Updates {}, num timesteps {}, FPS {} \n Last {} training episodes: mean/median reward {:.1f}/{:.1f}, min/max reward {:.1f}/{:.1f}\n".
-        #         format(j, total_num_steps,
-        #                int(total_num_steps / (end - start)),
-        #                len(episode_rewards),
-        #                np.mean(episode_rewards),
-        #                np.median(episode_rewards),
-        #                np.min(episode_rewards),
-        #                np.max(episode_rewards), dist_entropy,
-        #                value_loss, action_loss))
-
-    #     if (args.eval_interval is not None
-    #             and j % args.eval_interval == 0):
-    #         eval_envs = make_vec_envs(
-    #             args.env_name, args.seed + args.num_eval_processes, args.num_eval_processes,
-    #             args.gamma, eval_log_dir, args.add_timestep, device, True, 4, args.carl_wrapper)
-
-    #         # MsPacman no vecnormlize
-    #         vec_norm = get_vec_normalize(eval_envs)
-    #         assert(vec_norm is None)
-    #         if vec_norm is not None:
-    #             vec_norm.eval()
-    #             vec_norm.ob_rms = get_vec_normalize(envs).ob_rms
-
-    #         eval_episode_rewards = []
-
-    #         eval_recurrent_hidden_states = torch.zeros(args.num_eval_processes,
-    #                         actor_critic.recurrent_hidden_state_size, device=device)
-    #         eval_masks_device = torch.zeros(args.num_eval_processes, 1, device=device)
-    #         eval_g = torch.ones(args.num_eval_processes, 1)*tonic_g
-    #         eval_g_device = torch.ones(args.num_eval_processes, 1, device=device)*tonic_g
-    #         eval_evaluations = torch.zeros(args.num_eval_processes, 1)
-    #         obs = eval_envs.reset()
-    #         obs = obs_representation(obs, args.modulation, eval_g_device, args.input_neuro)
-
-    #         while len(eval_episode_rewards) < 10:
-    #             with torch.no_grad():
-    #                 value, action, _, eval_recurrent_hidden_states, _, _, _, _ , _= actor_critic.act(
-    #                     obs, eval_g_device, eval_recurrent_hidden_states, eval_masks_device, deterministic=True)
-
-    #             # Obser reward and next obs
-    #             obs, reward, done, infos = eval_envs.step(action)
-    #             obs = obs_representation(obs, args.modulation, eval_g_device, args.input_neuro)
-
-    #             eval_masks = torch.FloatTensor([[0.0] if done_ else [1.0]
-    #                                             for done_ in done])
-    #             #update eval_g
-    #             with torch.no_grad():
-    #                 eval_masks_device.copy_(eval_masks)
-    #                 next_value = actor_critic.get_value(obs, eval_g_device, eval_recurrent_hidden_states, eval_masks_device).detach()
-    #             eval_evaluations, eval_g = update_mode(eval_evaluations, eval_masks, reward, value, next_value, tonic_g, phasic_g, eval_g, args.phasic_threshold)
-    #             if args.modulation != 0:
-    #                 eval_g_device.copy_(eval_g)
-
-    #             for info in infos:
-    #                 if 'episode' in info.keys():
-    #                     eval_episode_rewards.append(info['episode']['r'])
-
-    #         eval_envs.close()
-    #         # print("eval scores are ", eval_episode_rewards)
-    #         mean_eval = np.mean(eval_episode_rewards)
-    #         if mean_eval > best_eval:
-    #             best_eval = mean_eval
-    #             save_model = actor_critic
-    #             if args.cuda:
-    #                 save_model = copy.deepcopy(actor_critic).cpu()
-    #             torch.save(save_model, os.path.join(save_path, args.env_name + ".pt"))
-    #         writer.add_scalar('data/eval_reward', mean_eval, j)
-    #         # print(" Evaluation using {} episodes: mean reward {:.5f}\n".
-    #         #     format(len(eval_episode_rewards),
-    #         #            np.mean(eval_episode_rewards)))
-
-    #     if args.vis and j % args.vis_interval == 0:
-    #         try:
-    #             # Sometimes monitor doesn't properly flush the outputs
-    #             win = visdom_plot(viz, win, args.log_dir, args.env_name,
-    #                               args.algo, args.num_frames)
-    #         except IOError:
-    #             pass
-    # print("best eval score is ", best_eval)
     writer.export_scalars_to_json("./all_scalars.json")
     writer.close()
 
