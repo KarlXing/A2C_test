c37db47524a335b33fe32f1afdcccca855eebfc2
Karl <jinweixing1006@gmail.com>
2019-04-27 00:18:30 -0700

    torch.sum() for two dimensional calc

--- main.py
+++ main.py
@@ -152,8 +152,8 @@ def main():
             num_nonzero = f_a.nonzero().size(0)
             mean_pos = mean_fa * num_feature_neurons / num_nonzero
             activation_ratio = f_a / mean_pos
-            num_bigger_mean_fa = sum(activation_ratio > 1).item()
-            num_bigger_half_fa = sum(activation_ratio > 0.5).item()
+            num_bigger_mean_fa = torch.sum(activation_ratio > 1).item()
+            num_bigger_half_fa = torch.sum(activation_ratio > 0.5).item()
             writer.add_scalar('analysis/fa_mean_ratio', (num_nonzero - num_bigger_mean_fa)/num_nonzero, g_step)
             writer.add_scalar('analysis/fa_0.5_ratio', (num_nonzero - num_bigger_mean_fa)/num_nonzero, g_step)
             writer.add_scalar('analysis/fa_active', num_nonzero/num_feature_neurons, g_step)
