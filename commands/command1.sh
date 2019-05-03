6e76af63258ce8f13a44dc4da1fbeddab8a7de26
Karl <jinweixing1006@gmail.com>
2019-05-03 13:22:58 -0700

    Add support to job

--- commands/command1.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-python3.6 -W ignore main.py --env-name "AssaultNoFrameskip-v0"  --num-frames  1000000  --carl-wrapper   --log-evaluation  --lr 1e-4  --complex-model  --reward-mode 0 --activation 0  --track-value-loss &
-python3.6 -W ignore main.py --env-name "AssaultNoFrameskip-v0"  --num-frames  1000000  --carl-wrapper   --log-evaluation  --lr 1e-4  --complex-model  --reward-mode 0 --activation 0  --track-value-loss
