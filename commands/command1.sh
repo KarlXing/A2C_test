fe85eb208b0f51364926fbbcbb91be5d0e42aca0
Karl <jinweixing1006@gmail.com>
2019-05-02 18:36:11 -0700

    echo date

--- commands/command1.sh
+++ commands/command1.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+date &
 python3.6 -W ignore main.py --env-name "AssaultNoFrameskip-v0"  --num-frames  100000  --carl-wrapper   --log-evaluation  --lr 1e-4  --complex-model  --reward-mode 0 --activation 0  --track-value-loss &
-sleep 2 &
+sleep 5 &
+date &
 python3.6 -W ignore main.py --env-name "AssaultNoFrameskip-v0"  --num-frames  100000  --carl-wrapper   --log-evaluation  --lr 1e-4  --complex-model  --reward-mode 0 --activation 0  --track-value-loss
