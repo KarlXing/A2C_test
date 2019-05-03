56d8d1e777b4e5b7f016e86d68ffbfa73c96f363
Karl <jinweixing1006@gmail.com>
2019-05-03 12:51:48 -0700

    Update command

--- commands/command3.sh
+++ commands/command3.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-(date; $1) &
-(sleep 5; date; $1) &
-(sleep 10; date; $1) &
-(echo $1 >> command) 
-cp command runs/
+(date; eval $1) &
+(sleep 5; date; eval $1) &
+(sleep 10; date; eval $1) &
+(echo $1 >> command);
+cp command  runs/
