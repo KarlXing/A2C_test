76470dcc24b07630ee4a0caa23410d528ec58169
Karl <jinweixing1006@gmail.com>
2019-05-04 14:01:24 -0700

    Test loop

--- commands/command3.sh
+++ commands/command3.sh
@@ -1,8 +1,30 @@
 #!/bin/sh
 
-(date; eval $1) &
-(sleep 5; date; eval $1) &
-(sleep 10; date; eval $1)
+
+if [ $2 == 1 ]; then
+    date; eval $1
+elif [ $2 == 2 ]; then
+    (date; eval $1) &
+    (sleep 5; date; eval $1)
+elif [ $2 == 3 ]; then
+    (date; eval $1) &
+    (sleep 5; date; eval $1) &
+    (sleep 10; date; eval $1)
+elif [ $2 == 4]; then
+    (date; eval $1) &
+    (sleep 5; date; eval $1) &
+    (sleep 10; date; eval $1) &
+    (sleep 15; date; eval $1)
+elif [ $2 == 5]; then
+    (date; eval $1) &
+    (sleep 5; date; eval $1) &
+    (sleep 10; date; eval $1) &
+    (sleep 15; date; eval $1) &
+    (sleep 20; date; eval $1)
+else; then
+    echo "Invalid Loop"
+fi
 
 echo $1 >> command
+git rev-parse --short HEAD  >> command
 cp command runs/
