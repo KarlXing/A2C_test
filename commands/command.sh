cffcbc426cb368f0e88b78018db427ba4d65738b
Karl <jinweixing1006@gmail.com>
2019-05-13 13:11:58 -0700

    Update command

--- commands/command.sh
+++ commands/command.sh
@@ -1,29 +1,11 @@
 #!/bin/sh
 
-if [ $2 = 1 ]; then
-    date; eval $1
-elif [ $2 = 2 ]; then
-    (date; eval $1) &
-    (sleep 5; date; eval $1)
-elif [ $2 = 3 ]; then
-    (date; eval $1) &
-    (sleep 5; date; eval $1) &
-    (sleep 10; date; eval $1)
-elif [ $2 = 4]; then
-    (date; eval $1) &
-    (sleep 5; date; eval $1) &
-    (sleep 10; date; eval $1) &
-    (sleep 15; date; eval $1)
-elif [ $2 = 5]; then
-    (date; eval $1) &
-    (sleep 5; date; eval $1) &
-    (sleep 10; date; eval $1) &
-    (sleep 15; date; eval $1) &
-    (sleep 20; date; eval $1)
-else
-    echo "Invalid Loop"
-fi
 
-echo $1 >> command
+(date; python3.6 -W ignore main.py --env-name $1  --num-frames  100000000  --carl-wrapper   --log-evaluation  --lr 1e-4  --complex-model  --reward-mode 0 --activation 0  --track-value-loss) &
+(sleep 5; date; python3.6 -W ignore main.py --env-name $2  --num-frames  100000000  --carl-wrapper   --log-evaluation  --lr 1e-4  --complex-model  --reward-mode 0 --activation 0  --track-value-loss)
+(sleep 10; date; python3.6 -W ignore main.py --env-name $3 --num-frames  100000000  --carl-wrapper   --log-evaluation  --lr 1e-4  --complex-model  --reward-mode 0 --activation 0  --track-value-loss)
+
+
+echo $1 $2 $3 >> command
 git rev-parse --short HEAD  >> command
 cp command runs/
