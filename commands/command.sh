9addecefa5d74b9ec166c7320d5ac2b335218fe9
Karl <jinweixing1006@gmail.com>
2019-05-13 13:23:38 -0700

    Three together

--- commands/command.sh
+++ commands/command.sh
@@ -2,7 +2,7 @@
 
 
 (date; python3.6 -W ignore main.py --env-name $1  --num-frames  100000000  --carl-wrapper   --log-evaluation  --lr 1e-4  --complex-model  --reward-mode 0 --activation 0  --track-value-loss) &
-(sleep 5; date; python3.6 -W ignore main.py --env-name $2  --num-frames  100000000  --carl-wrapper   --log-evaluation  --lr 1e-4  --complex-model  --reward-mode 0 --activation 0  --track-value-loss)
+(sleep 5; date; python3.6 -W ignore main.py --env-name $2  --num-frames  100000000  --carl-wrapper   --log-evaluation  --lr 1e-4  --complex-model  --reward-mode 0 --activation 0  --track-value-loss) &
 (sleep 10; date; python3.6 -W ignore main.py --env-name $3 --num-frames  100000000  --carl-wrapper   --log-evaluation  --lr 1e-4  --complex-model  --reward-mode 0 --activation 0  --track-value-loss)
 
 
