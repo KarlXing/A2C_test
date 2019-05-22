0c476ac82ac27a4c631c4d8fb5a496b6f76d9750
Karl <jinweixing1006@gmail.com>
2019-05-22 11:03:57 -0700

    Update command

--- commands/command.sh
+++ commands/command.sh
@@ -1,39 +1,21 @@
 #!/bin/sh
 
-if [ $2 = 1 ]; then
-    date; eval $1
-elif [ $2 = 2 ]; then
-    (date; eval $1) &
-    (sleep 5; date; eval $1)
-elif [ $2 = 3 ]; then
-    (date; eval $1) &
-    (sleep 5; date; eval $1) &
-    (sleep 10; date; eval $1)
-elif [ $2 = 4 ]; then
-    (date; eval $1) &
-    (sleep 5; date; eval $1) &
-    (sleep 10; date; eval $1) &
-    (sleep 15; date; eval $1)
-elif [ $2 = 5 ]; then
-    (date; eval $1) &
-    (sleep 5; date; eval $1) &
-    (sleep 10; date; eval $1) &
-    (sleep 15; date; eval $1) &
-    (sleep 20; date; eval $1)
-elif [ $2 = 9 ]; then
-    (date; eval $1) &
-    (sleep 5; date; eval $1) &
-    (sleep 10; date; eval $1) &
-    (sleep 15; date; eval $1) &
-    (sleep 20; date; eval $1) &
-    (sleep 25; date; eval $1) &
-    (sleep 30; date; eval $1) &
-    (sleep 35; date; eval $1) &
-    (sleep 40; date; eval $1)
-else
-    echo "Invalid Loop"
-fi
+command1="python3.6 -W ignore  --env-name $1 --num-frames  $2  --log-evaluation  --lr 1e-4  --reward-mode 0" 
+command2="python3.6 -W ignore  --env-name $1 --num-frames  $2  --log-evaluation  --lr 1e-4  --reward-mode 1" 
+command3="python3.6 -W ignore  --env-name $1 --num-frames  $2  --log-evaluation  --lr 1e-4  --reward-mode 2" 
 
-echo $1 >> command
+(date; eval $command1) &
+(sleep 5; date; eval $command1) &
+(sleep 10; date; eval $command1) &
+(sleep 15; date; eval $command2) &
+(sleep 20; date; eval $command2) &
+(sleep 25; date; eval $command2) &
+(sleep 30; date; eval $command3) &
+(sleep 35; date; eval $command3) &
+(sleep 40; date; eval $command3)
+
+echo $command1 >> command
+echo $command2 >> command
+echo $command3 >> command
 git rev-parse --short HEAD  >> command
 cp command runs/
