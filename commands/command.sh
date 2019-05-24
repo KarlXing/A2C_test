b1f1b7e81472cb65fb2c060d4a4846f846ba24ff
Karl <jinweixing1006@gmail.com>
2019-05-24 09:56:30 -0700

    Add echo

--- commands/command.sh
+++ commands/command.sh
@@ -4,15 +4,15 @@ command1="python3.6 -W ignore main.py --env-name $1 --num-frames  $2  --log-eval
 command2="python3.6 -W ignore main.py --env-name $1 --num-frames  $2  --log-evaluation  --lr 1e-4  --reward-mode 1  --track-value-loss" 
 command3="python3.6 -W ignore main.py --env-name $1 --num-frames  $2  --log-evaluation  --lr 1e-4  --reward-mode 2  --track-value-loss" 
 
-(date; eval $command1) &
-(sleep 5; date; eval $command1) &
-(sleep 10; date; eval $command1) &
-(sleep 15; date; eval $command2) &
-(sleep 20; date; eval $command2) &
-(sleep 25; date; eval $command2) &
-(sleep 30; date; eval $command3) &
-(sleep 35; date; eval $command3) &
-(sleep 40; date; eval $command3)
+(date; eval $command1; echo "done") &
+(sleep 5; date; eval $command1; echo "done") &
+(sleep 10; date; eval $command1; echo "done") &
+(sleep 15; date; eval $command2; echo "done") &
+(sleep 20; date; eval $command2; echo "done") &
+(sleep 25; date; eval $command2; echo "done") &
+(sleep 30; date; eval $command3; echo "done") &
+(sleep 35; date; eval $command3; echo "done") &
+(sleep 40; date; eval $command3; echo "done")
 
 wait 
 
