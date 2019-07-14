6d487c0ae544130a6d7ab0021579e7551ec0832e
Karl <jinweixing1006@gmail.com>
2019-07-14 12:03:59 -0700

    Update command

--- commands/command.sh
+++ commands/command.sh
@@ -1,23 +1,22 @@
 #!/bin/sh
 
-command1="python3.6 -W ignore main.py --env-name $1 --num-frames  $2  --log-evaluation  --lr 1e-3  --reward-mode $3  --track-value-loss" 
-command2="python3.6 -W ignore main.py --env-name $1 --num-frames  $2  --log-evaluation  --lr 1e-4  --reward-mode $3  --track-value-loss" 
-command3="python3.6 -W ignore main.py --env-name $1 --num-frames  $2  --log-evaluation  --lr 1e-5  --reward-mode $3  --track-value-loss" 
+command1="python3.6 -W ignore main.py --env-name $2 --num-frames  $1  --log-evaluation   --track-value-loss  --track_grad" 
+command2="python3.6 -W ignore main.py --env-name $3 --num-frames  $1  --log-evaluation   --track-value-loss  --track_grad" 
+command3="python3.6 -W ignore main.py --env-name $4 --num-frames  $1  --log-evaluation   --track-value-loss  --track-grad" 
+command4="python3.6 -W ignore main.py --env-name $5 --num-frames  $1  --log-evaluation   --track-value-loss  --track-grad" 
 
 (date; eval $command1; echo "done") &
 (sleep 5; date; eval $command1; echo "done") &
-(sleep 10; date; eval $command1; echo "done") &
-(sleep 15; date; eval $command2; echo "done") &
-(sleep 20; date; eval $command2; echo "done") &
-(sleep 25; date; eval $command2; echo "done") &
-(sleep 30; date; eval $command3; echo "done") &
-(sleep 35; date; eval $command3; echo "done") &
-(sleep 40; date; eval $command3; echo "done")
+(sleep 10; date; eval $command2; echo "done") &
+(sleep 15; date; eval $command2; echo "done")
+
 
 wait 
 echo "mean"
 echo $command1 >> command
 echo $command2 >> command
 echo $command3 >> command
+echo $command4 >> command
+
 git rev-parse --short HEAD  >> command
 cp command runs/
